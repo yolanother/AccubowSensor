a6944e5bcdc911b8efd2987c546b68a44616113d
Yolan <yolanother@gmail.com>
2021-06-26 21:45:58 -0700

    Working client reconnect

--- sensor-server.py
+++ sensor-server.py
@@ -3,32 +3,60 @@
 import socketserver
 from socket import *
 import time
+import threading
 from lib import VL53L0X
+
+
+class Data():
+    distance = -1
+    client = None
+    socket = None
+
+data = Data()
+
+# Create a VL53L0X object
+tof = VL53L0X.VL53L0X()
+
+# Start ranging
+tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+
+timing = tof.get_timing()
+if (timing < 20000):
+    timing = 20000
+
+def trackRange(data):
+    while True:
+        lastDistance = data.distance
+        data.distance = tof.get_distance()
+        print("\r>> %d mm, %d cm" % (data.distance, (data.distance/10)), end="")
+
+        if(lastDistance != data.distance and data.client != None):
+            print("\r>> %d mm, %d cm: Sending to %s" % (data.distance, (data.distance/10), data.client[0]), end="")
+            data.socket.sendto(data.distance.to_bytes(4, 'little'), data.client)
+        
+        time.sleep(timing/1000000.00)
+
+thread = threading.Thread(target=trackRange, args=(data,))
+thread.daemon = True                            # Daemonize thread
+thread.start()
 
 class MyHandler(socketserver.BaseRequestHandler):
     def handle(self):
         print("Client {} connected. Sending sensor data".format(self.client_address[0]))
         socket = self.request[1]
 
-        # Create a VL53L0X object
-        tof = VL53L0X.VL53L0X()
+        data.socket = socket
+        data.client = self.client_address
 
-        # Start ranging
-        tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
-
-        timing = tof.get_timing()
-        if (timing < 20000):
-            timing = 20000
-        
-        while True:
-            distance = tof.get_distance()
-            if (distance > 0 and distance < 8190):
-                print ("%d mm, %d cm" % (distance, (distance/10)))
-                socket.sendto(distance.to_bytes(4, 'little'), self.client_address)
-            time.sleep(timing/1000000.00)
+        #lastDistance = -1
+        #while(True):
+        #    if (data.distance > 0 and data.distance != lastDistance):
+        #        
+        #    lastDistance = data.distance
+        #    time.sleep(timing/1000000.00)
 
 print("Server started on port 8881")
 server = socketserver.UDPServer(('',8881), MyHandler)
 server.max_packet_size = 4
-server.serve_forever(  )
+server.serve_forever()
 
--- sensor-test-client.py
+++ sensor-test-client.py
@@ -1,12 +1,13 @@
 import socket
 import time
+import struct
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 s.sendto(bytes("connect", "utf-8"), ("192.168.1.159", 8881))
 while True:
-    data = s.recv(32)
+    data = s.recv(4)
     if(len(data) > 0):
-        print ("Received: %d" % int(data))
+        print (">>\r Received: %d" % int.from_bytes(data, byteorder='little'), end='')
 
 s.close()
